2bbe38156e55897a0044323ff32a0c30ee0e9e2c
Pedro Abreu <pedrolucasornellas69@gmail.com>
2025-02-08 23:12:41 -0300

    Feat: Melhora na funcionalidade de notas salvas.

--- scripts/notasSalvas.js
+++ scripts/notasSalvas.js
@@ -4,27 +4,39 @@ let sequenciasSalvas;
 
 
 function salvaSequencias(){
+    const notasSelecionadas = obterNotasSelecionadas();
+
     if(!sequenciasSalvas){
         sequenciasSalvas = JSON.parse(localStorage.getItem('sequenciasSalvas')) || [];
     }
-    const notasSelecionadas = obterNotasSelecionadas();
+    if(sequenciasSalvas.length >= 5) sequenciasSalvas.shift();
+    
     if(notasSelecionadas.length > 2) sequenciasSalvas.push(notasSelecionadas);
     else alert('Pelo menos três notas devem estar selecionadas');
     
     localStorage.setItem('sequenciasSalvas', JSON.stringify(sequenciasSalvas));
     console.log(sequenciasSalvas);
+    exibirNotasSalvas();
 }
 
 function exibirNotasSalvas(){
+    sequenciasSalvasDiv.innerHTML = '';
     sequenciasSalvas = JSON.parse(localStorage.getItem('sequenciasSalvas'));
     if(!sequenciasSalvas){
         sequenciasSalvasDiv.innerHTML = 'Não há sequências salvas'
     }else{
         sequenciasSalvas.forEach(sequencia => {
+            sequenciaASerInserida = document.createElement('div');
+            sequenciaASerInserida.classList= 'sequencia-salvas-item';
             sequencia.forEach(nota =>{
-                sequenciasSalvasDiv.innerHTML += nota + " "
+                notaDiv = document.createElement('div')
+                notaDiv.classList = 'sequencia-salvas-nota'
+                notaDiv.innerHTML = nota;
+                sequenciaASerInserida.append(notaDiv);
+
+                sequenciasSalvasDiv.append(sequenciaASerInserida)
+                // sequenciasSalvasDiv.innerHTML += nota + " "
             })
-            sequenciasSalvasDiv.innerHTML += '<br>'
         });
         // sequenciasSalvasDiv.innerHTML = 'Suas sequências'
     }
