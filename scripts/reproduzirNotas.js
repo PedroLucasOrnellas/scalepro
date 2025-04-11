3014b7435b61cfea9e1dd6ff51f4086e455f6350
Pedro Abreu <pedrolucasornellas69@gmail.com>
2025-04-11 20:43:30 -0300

    New: Cria outro modo de carregamento de audios, com audios carregados diretamente no html.

--- scripts/reproduzirNotas.js
+++ scripts/reproduzirNotas.js
@@ -1,19 +1,30 @@
 const audios = document.querySelectorAll('.audios audio');
-const audiosArray = Array.from(audios);
+const audiosArray = Array.from(audios).map(audio => audio.getAttribute('id'));
+let audioInstance = null;
 
-function reproduzirNotas(notaElement){
+function reproduzirNotas(notaElement) {
     let indexAudios;
-    const nota = notaElement.getAttribute('data-name');
 
-     if(nota.includes('b')) {
-        indexAudios = audiosArray.indexOf(nota) !== -1 ? audiosArray.indexOf(nota) - 1 : 'not-found';
+    // Obtém o nome da nota do elemento
+    let nota = notaElement.getAttribute('data-name').replace('b', 'ant').toLowerCase().replace('#', 'Sus');
+
+    if(nota.includes('ant')) {
+        let notaX = nota.toUpperCase().replace('ANT', 'b');
+        indexAudios = notasArray.indexOf(notaX) !== -1 ? notasArray.indexOf(notaX) - 1 : 'not-found';
+        nota = notasArray[indexAudios].toLowerCase().replace('#', 'Sus');
     }
-    console.log(indexAudios);
-    
-    // Obter o nome da nota a partir do atributo data-name
-    // let notaSymbol = nota.getAttribute('data-name').toLowerCase().replace('#', 'Sus');
-    // if(indexAudios)
-    //     notaSymbol = notasArray[indexAudios].toLowerCase().replace('#', 'Sus');
+
+    // Para o áudio anterior se houver
+    if (audioInstance) {
+        audioInstance.pause();
+        audioInstance.currentTime = 0;
+    }
+
+    // Toca o novo áudio e armazena a instância atual
+    audioInstance = document.querySelector(`audio[id="${nota}"]`);
+    if (audioInstance) {
+        audioInstance.play();
+    }
+
     console.log(nota);
-    console.log(audios)
 }
